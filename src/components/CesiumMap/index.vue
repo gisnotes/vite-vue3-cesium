f5436d1a7967cc0f646e1603c071c06e67260358
lovewhoilove <3528300287@qq.com>
2024-11-29 00:41:36 +0800

    笛卡尔坐标系

--- src/components/CesiumMap/index.vue
+++ src/components/CesiumMap/index.vue
@@ -2,14 +2,12 @@
 import {
   Ion,
   Viewer,
-  Terrain,
-  Cesium3DTileset,
+  DebugModelMatrixPrimitive,
   Cartesian3,
-  HeadingPitchRoll,
-  CesiumTerrainProvider,
-  createOsmBuildingsAsync,
-  Math,
-  Matrix4
+  ScreenSpaceEventType,
+  Color,
+  Entity,
+  ArcType,
 } from "cesium";
 
 import useCSViewerStore from "@/stores/csViewer.js";
@@ -31,33 +29,11 @@ const cesiumBaseUrl =
     : sourceCesiumBaseUrl;
 window.CESIUM_BASE_URL = cesiumBaseUrl;
 
-const props = defineProps({
-  mapType: {
-    type: String,
-    default: "tdtImg"
-  }
-});
-
-const emits = defineEmits(["viewerCreated", "leftClick"]);
-
-const loading = ref(true);
-const value = ref("");
-
-const options = [
-  { value: "1", label: "旧金山渡轮大厦" },
-  { value: "2", label: "39号码头" },
-  { value: "3", label: "Skyline" },
-  { value: "4", label: "伦巴底街" },
-  { value: "5", label: "丹奈利峰" },
-  { value: "6", label: "安克拉治" },
-  { value: "7", label: "山峰(群峰)" },
-  { value: "8", label: "河床" },
-  { value: "9", label: "OSM建筑物数据" },
-];
+const emits = defineEmits(["viewerCreated"]);
 
 onMounted(() => {
   initViewer();
-  add3DModelData();
+  addThreeDimensionalCartesianCoordinateSystem(); //添加显示三维笛卡尔空间直角坐标系
 });
 
 /**
@@ -78,160 +54,144 @@ function initViewer() {
     selectionIndicator: false, //是否显示选取指示器
     skyBox: false, //skyBox、Sun or Moon都不会被添加
     shouldAnimate: true, //是否允许动画
-    showRenderLoopErrors: false, //如果为true，则如果出现渲染循环错误，此小部件将自动向用户显示包含错误的HTML面板。
-    terrain: Terrain.fromWorldTerrain()
+    showRenderLoopErrors: false, //如果为true，则如果出现渲染循环错误，此小部件将自动向用户显示包含错误的HTML面板
   });
   csViewerStore.viewer = viewerRef;
   viewerRef.value.cesiumWidget.creditContainer.style.display = "none"; //隐藏版权信息
   viewerRef.value.scene.debugShowFramesPerSecond = true; //添加帧速显示
+  viewerRef.value.camera.flyTo({
+    destination: Cartesian3.fromDegrees(45.0, 20.0, 17850000),
+  });
+  //取消默认的双击事件
+  viewerRef.value.cesiumWidget.screenSpaceEventHandler.removeInputAction(
+    ScreenSpaceEventType.LEFT_DOUBLE_CLICK
+  );
+  viewerRef.value.scene.globe.depthTestAgainstTerrain = true; //开启深度测试
+  // 抗锯齿
+  viewerRef.value.scene.fxaa = true;
+  viewerRef.value.scene.postProcessStages.fxaa.enabled = false;
+  /**
+   * 设置渲染分辨率的缩放因子，默认值为1，详细解释见官方api文档：https://cesium.com/learn/cesiumjs/ref-doc/Viewer.html?classFilter=viewer#resolutionScale
+   * devicePixelRatio表示：显示设备的物理像素分辨率与CSS像素分辨率之比
+   */
+  viewerRef.value.resolutionScale = window.devicePixelRatio;
+  //开启地表透明
+  viewerRef.value.scene.globe.translucency.enabled = true;
+  viewerRef.value.scene.globe.translucency.frontFaceAlpha = 0.5;
   emits("viewerCreated");
 }
 
-/**
- * 添加Cesium Ion中提供的三维模型数据
- */
-function add3DModelData() {
-  addAerometrexSanFrancisco3DModel();
-  addArcticDEM();
-  addOsmBuildings();
-  if (loading.value) loading.value = false;
+function addThreeDimensionalCartesianCoordinateSystem() {
+  //添加坐标轴
+  const DEFAULT_AXIS_LENGTH = 10000000.0;
+  viewerRef.value.scene.primitives.add(
+    new DebugModelMatrixPrimitive({
+      length: DEFAULT_AXIS_LENGTH, //坐标轴的长度，单位是米。默认值为10000000.0
+      width: 10.0, //坐标轴的宽度，单位是像素，默认值为2.0
+    })
+  );
+  const LabelHeight = DEFAULT_AXIS_LENGTH * 1.02;
+  //添加原点文字
+  // addCoordinateLabel(Cartesian3.ZERO, "原点");
+  const ZERO_OFFSET = -0.03 * DEFAULT_AXIS_LENGTH;
+  const zeroLabel = createLabel(
+    new Cartesian3(ZERO_OFFSET, ZERO_OFFSET, ZERO_OFFSET),
+    "原点"
+  );
+  viewerRef.value.entities.add(zeroLabel);
+  //添加坐标轴x文字标签
+  const xLabel = createLabel(new Cartesian3(LabelHeight, 0.0, 0.0), "x");
+  viewerRef.value.entities.add(xLabel);
+  //添加坐标轴y文字标签
+  const yLabel = createLabel(new Cartesian3(0.0, LabelHeight, 0.0), "y");
+  viewerRef.value.entities.add(yLabel);
+  //添加坐标轴z文字标签
+  const zLabel = createLabel(new Cartesian3(0.0, 0.0, LabelHeight), "z");
+  viewerRef.value.entities.add(zLabel);
+  //绘制赤道线，即0度纬线
+  const equator = createParallel(0, Color.BLUE);
+  viewerRef.value.entities.add(equator);
+  //添加赤道线文字标签
+  const equatorLabel = createLabel(
+    Cartesian3.fromDegrees(45.0, 0.0, 300000.0),
+    "赤道"
+  );
+  viewerRef.value.entities.add(equatorLabel);
+  //绘制本初子午线，即0度经线
+  const primeMeridian = createMeridian(0, Color.RED);
+  viewerRef.value.entities.add(primeMeridian);
+  //添加本初子午线文字标签
+  const primeMeridianLabel = createLabel(
+    Cartesian3.fromDegrees(0.0, 20.0, 300000.0),
+    "本初子午线"
+  );
+  viewerRef.value.entities.add(primeMeridianLabel);
+  //添加90度经线
+  const meridian90 = createMeridian(90, Color.YELLOW);
+  viewerRef.value.entities.add(meridian90);
+  //添加90度经线
+  const meridian90Label = createLabel(Cartesian3.fromDegrees(90.0, 20.0, 300000.0),
+    "90度经线");
+  viewerRef.value.entities.add(meridian90Label);
+}
+
+//添加坐标轴标签文字标签
+function createLabel(position, text) {
+  return {
+    position,
+    label: { text },
+  };
 }
 
 /**
- * Aerometrex（一家提供3D建模、航空影像和LiDAR等地理空间技术的服务的澳大利亚公司），
- * 这里加载的是旧金山高分辨率3D模型，带有街道增强的3D模型
+ * 绘制纬线圈
+ * @param {number} latitude 维度
+ * @param {Color} color 材质颜色
+ * @returns {Entity} 返回实体对象
  */
-async function addAerometrexSanFrancisco3DModel() {
-  try {
-    const tileset = await Cesium3DTileset.fromIonAssetId(1415196, {
-      maximumScreenSpaceError: 4
-    });
-    viewerRef.value.scene.primitives.add(tileset);
-  } catch (error) {
-    ElMessage.error(`Error creating tileset: ${error}`);
-    if (loading.value) loading.value = false;
-  }
+function createParallel(latitude, color) {
+  return {
+    polyline: {
+      positions: Cartesian3.fromDegreesArray([
+        -180,
+        latitude,
+        -90,
+        latitude,
+        0,
+        latitude,
+        90,
+        latitude,
+        180,
+        latitude,
+      ]),
+      width: 2,
+      arcType: ArcType.RHUMB,
+      material: color,
+    },
+  };
 }
 
-/**
- * 来自Arctic DEM项目（Release 4）的高分辨率北极地形
- * (https://www.pgc.umn.edu/data/arcticdem/)
- */
-async function addArcticDEM() {
-  try {
-    viewerRef.value.scene.terrainProvider =
-      await CesiumTerrainProvider.fromIonAssetId(3956);
-  } catch (error) {
-    ElMessage.error(`Failed to load terrain. ${error}`);
-    if (loading.value) loading.value = false;
-  }
-}
-
-/**
- * OSM Buildings
- */
-async function addOsmBuildings() {
-  try {
-    const osmBuildingsTileset = await createOsmBuildingsAsync();
-    viewerRef.value.scene.primitives.add(osmBuildingsTileset);
-  } catch (error) {
-    ElMessage.error(`Error creating tileset: ${error}`);
-    if (loading.value) loading.value = false;
-  }
-}
-
-/**
- * 更换模型地点
- */
-function handleChange(val) {
-    let dest, ori;
-    if (val === "1") {
-      //Ferry Building（旧金山渡轮大厦，旧金山市中心的一座历史建筑）：街道级增强网格，每像素分辨率为6毫米
-      dest = [-2703541.419456986, -4261164.971874713, 3887416.257562123];
-      ori = [5.959123393581913, -0.03131876941215883, 0.0000033030489428043097];
-    } else if (val === "2") {
-      //Pier 39（39号码头，旧金山渔人码头的旅游景点和购物中心）：每像素分辨率为2cm
-      dest = [-2704263.584923937, -4259336.981155519, 3888978.5732662966];
-      ori = [
-        2.4287691459386607, -0.49459905591668996, 0.0000029701571779838787
-      ];
-    } else if (val === "3") {
-      //Skyline:（建筑物或群山在天空映衬下的）轮廓线：每像素分辨率为2cm
-      dest = [-2702979.5635104137, -4261981.190435306, 3887092.144148863];
-      ori = [4.534265054628527, -0.08846186652294352, 0.0000075141499165098935];
-    } else if (val === "4") {
-      //Lombard Street（旧金山的伦巴底街）:：每像素分辨率为2cm
-      dest = [-2705631.6783492276, -4259449.36938678, 3887903.89229016];
-      ori = [5.999439616451804, -0.20513082834763674, 4.7213266807233367e-7];
-    } else if (val === "5") {
-      //Denali：丹奈利峰（阿拉斯加州的山）
-      dest = Cartesian3.fromRadians(
-        -2.6399828792482234,
-        1.0993550795541742,
-        5795
-      );
-      ori = [3.8455, -0.4535, 0.0];
-    } else if (val === "6") {
-      //Anchorage Area：安克拉治是美国阿拉斯加州最大城市
-      dest = Cartesian3.fromRadians(
-        -2.610708034601548,
-        1.0671172431736584,
-        1900
-      );
-      ori = [4.6, -0.341, 0.0];
-    } else if (val === "7") {
-      //Peaks：山峰(群峰)
-      dest = Cartesian3.fromRadians(
-        -2.6928866820212813,
-        1.072394255273859,
-        3700
-      );
-      ori = [1.6308222948889464, -0.6473480165020193, 0.0];
-    } else if (val === "8") {
-      //Riverbed：河床
-      dest = Cartesian3.fromRadians(
-        -2.6395623497608596,
-        1.0976443174490356,
-        2070
-      );
-      ori = [6.068794108659519, -0.08514161789475816, 0.0];
-    } else if (val === "9") {
-      //OSM Buildings
-      dest = Cartesian3.fromDegrees(-74.019, 40.6912, 750);
-      ori = [Math.toRadians(20), Math.toRadians(-20), 0.0];
-    }
-    flyToSomewhere(dest, ori);
-}
-
-function flyToSomewhere(dest, ori) {
-  viewerRef.value.scene.camera.flyTo({
-    destination: dest instanceof Cartesian3 ? dest : new Cartesian3(...dest),
-    orientation: new HeadingPitchRoll(...ori),
-    duration: 1
-  });
+function createMeridian(longitude, color) {
+  return {
+    polyline: {
+      positions: Cartesian3.fromDegreesArray([
+        longitude,
+        90,
+        longitude,
+        0,
+        longitude,
+        -90,
+      ]),
+      width: 2,
+      arcType: ArcType.RHUMB,
+      material: color,
+    },
+  };
 }
 </script>
 
 <template>
-  <div
-    class="map"
-    ref="viewerDivRef"
-    v-loading="loading"
-    element-loading-text="三维模型加载中..."
-    element-loading-background="rgba(122, 122, 122, 0.8)">
-    <el-select
-      class="select"
-      v-model="value"
-      placeholder="请选择模型"
-      size="default"
-      @change="handleChange">
-      <el-option
-        v-for="item in options"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value" />
-    </el-select>
-  </div>
+  <div class="map" ref="viewerDivRef"></div>
 </template>
 
 <style lang="scss" scoped>
